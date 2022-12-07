1e6f105e9a89faf645269f6c4a8d03b60075c454
PoulLorca <lorcapoul@gmail.com>
2022-12-07 09:48:11 -0300

    retry http get

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -25,6 +25,8 @@ export class ProductsComponent implements OnInit {
     },
     description:''
   };
+  limit = 10;
+  offset = 0;
 
 
   constructor(
@@ -35,10 +37,7 @@ export class ProductsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.productsService.getAllProducts()
-    .subscribe(data => {
-      this.products = data;
-    });
+    this.loadMore()
   }
 
   onAddToShoppingCart(product: Product) {
@@ -95,6 +94,14 @@ export class ProductsComponent implements OnInit {
       this.products.splice(productIndex, 1);
       this.showProductDetail = false;
     })
+  }
+
+  loadMore(){
+    this.productsService.getProductsByPage(this.limit, this.offset)
+    .subscribe(data => {
+      this.products = this.products.concat(data);
+      this.offset += this.limit;
+    });
   }
 
 }
