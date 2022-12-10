72ede6771e513be1dcd7766125d3a5a97231a1ef
PoulLorca <lorcapoul@gmail.com>
2022-12-10 18:24:27 -0300

    Can deactivate

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -2,43 +2,34 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
-import { Router } from '@angular/router';
-import { TokenService } from '../services/token.service';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AdminGuard implements CanActivate {
 
   constructor(
-    private tokenService: TokenService,
     private authService: AuthService,
     private router: Router
-  ) {
-  }
+  ){}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    //  const token = this.tokenService.getToken();
-    //  if (!token) {
-    //    this.router.navigate(['/home'])
-    //    return false
-    //  }
-    //  return true;
-
-    return this.authService.user$
+      return this.authService.user$
       .pipe(
         map(user => {
-          if (!user) {
+          if (user?.role === 'admin') {            
+            return true
+          }else{
             this.router.navigate(['/home'])
             return false
-          }
-          return true;
+          }          
         })
       )
   }
-
+  
 }
--- src/app/website/pages/register/register.component.ts
+++ src/app/website/pages/register/register.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+import { onExit } from 'src/app/guards/exit.guard';
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, onExit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onExit(){
+    const confirm = Swal.fire({
+      title: 'Are you sure?',
+      showDenyButton: true,
+      confirmButtonText: 'Yes',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        return true;
+      }
+      return false
+    });
+    return confirm
+  }
 }
